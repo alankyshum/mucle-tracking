dfa325fae4ff23a699c1b29f3a3bdab4908d6243
Alan Shum <alan200994@gmail.com>
2016-06-19 11:27:12 +0800

    added navbar

--- public/js/main.js
+++ public/js/main.js
@@ -13,3 +13,23 @@ $(() => {
     muscleImg.classList.remove('hover');
   });
 });
+
+/**
+ * SETTING DEFAULT VALUES
+ */
+Date.prototype.toDateInputValue = (function() {
+  var local = new Date(this);
+  local.setMinutes(this.getMinutes() - this.getTimezoneOffset());
+  return local.toJSON().slice(0,10);
+});
+Date.prototype.toTimeInputValue = (function() {
+  var local = new Date(this);
+  var stamps = local.toLocaleTimeString().split(':');
+  stamps[0] = stamps[0].length===1 ? `0${stamps[0]}` : stamps[0];
+  stamps[1] = stamps[1].length===1 ? `0${stamps[1]}` : stamps[1];
+  return `${stamps[0]}:${stamps[1]}`;
+});
+
+const now = new Date();
+document.getElementById('date').value = now.toDateInputValue();
+document.getElementById('time').value = now.toTimeInputValue();
